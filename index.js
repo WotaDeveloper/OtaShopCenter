6bd786ad632f5782e7ce0f2e2700d1560c049eb5
lebrancconvas <lebrancconvas@gmail.com>
2018-06-06 02:50:17 +0700

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+    el: '#app',
+    data : {
+        name : "BNK48 Photoset",
+        price : 0
+    },
+    methods : {
+        confirm: function(){
+            alert("ยืนยันที่จะขายสินค้า ?")
+        }
+    }
+});
